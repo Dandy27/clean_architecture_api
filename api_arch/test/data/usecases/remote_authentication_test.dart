505691a4def272a2006a5257eadc2dbdf4b68da8
dandy <danielsbarbosa27@gmail.com>
2021-12-26 20:03:08 -0300

    ajustes no tests

--- api_arch/test/data/usecases/remote_authentication_test.dart
+++ api_arch/test/data/usecases/remote_authentication_test.dart
@@ -10,7 +10,7 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
   Future<void>? auth() async {
-    await httpClient.request(url: url, method: 'post'); 
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
@@ -21,14 +21,24 @@ abstract class HttpClient {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  late RemoteAuthentication sut;
+  late HttpClientSpy httpClient;
+  late String url;
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
   test('Should call HttpClient with correct values', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
     await sut.auth();
 
-    verify(httpClient.request(url: url, method: 'post',));
-  }); 
-  
-
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
+  });
 }
